975a70355078c60e829508441615fdd2320e84bb
Afroz quraishi <afroziiitian@gmail.com>
2021-03-05 15:28:16 +0530

    todo app completed

---
+++ src/ListItems.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ListItems.css';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item =>
+   {
+        return <div className="list" key={item.key}>
+        <p>
+            <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+                props.setUpdate(e.target.value,item.key)}}/>
+            <span>
+                <DeleteIcon className="mui" onClick={()=>props.deleteItem(item.key)}/>
+            </span>
+        </p>  
+        </div>
+    })
+    return <div>
+        {listItems}    
+    </div>;
+  }
+export default ListItems;
